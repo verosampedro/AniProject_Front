4fe8020f153955fe5d8c3f770d5da038cbc6b87a
Vero-Sampedro <verosampedro@icloud.com>
2024-03-23 14:51:11 +0100

    login done

--- src/components/login/LoginForm.vue
+++ src/components/login/LoginForm.vue
@@ -1,34 +1,60 @@
-<script>
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+import { useAuthStore } from "../../stores/authStore.js";
+import { useRouter } from 'vue-router';
+
+const uri = 'http://localhost:8080/api/v1';
+
+const username = ref('');
+const password = ref('');
+
+const authStore = useAuthStore();
+const router = useRouter(); 
+
+const login = async () => {
+  try {
+    const response = await axios.get(`${uri}/login`, {
+      auth: {
+            username: username.value,
+            password: password.value
+          },
+          withCredentials: true
+        },
+        );
+        const data = response.data;
+        const userRole = data.roles;
+
+        authStore.userRole = userRole;
+
+        authStore.isAuthenticated = true;
+
+        console.log(authStore.userRole)
+        console.log(authStore.isAuthenticated)
+
+        router.push('/requests');
+
+      } catch (error) {
+        console.error(error);
+      }
+};
 
 </script>
 
 <template>
-
-    <div class="login-form">
-
-      <h2>Login</h2>
-
-      <form @submit.prevent="login">
-
-        <div class="form-group">
-          
-          <input type="text" id="username" v-model="username" placeholder="Username" required>
-
-        </div>
-
-        <div class="form-group">
-          
-          <input type="password" id="password" v-model="password" placeholder="Password" required>
-
-        </div>
-
-        <button type="submit">LOGIN</button>
-
-      </form>
-
-    </div>
-
-  </template>
+  <div class="login-form">
+     <h2>Login</h2>
+     <form @submit.prevent="login">
+       <div class="form-group">
+         <input type="text" id="username" v-model="username" placeholder="Username" required>
+       </div>
+       <div class="form-group">
+         <input type="password" id="password" v-model="password" placeholder="Password" required>
+       </div>
+       <button type="submit">LOGIN</button>
+     </form>
+  </div>
+ </template>
 
 <style lang="scss" scoped>
 
